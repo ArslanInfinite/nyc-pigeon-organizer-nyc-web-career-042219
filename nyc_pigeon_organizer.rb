9869f1fbd10cd608dab00fc07f00c99284ba626a
Arslan Ali <ali.arslan319@gmail.com>
2019-04-15 22:44:50 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,11 +1,11 @@
 def nyc_pigeon_organizer(data)
   pigeon_hierarchy = {}
   data.each do |category, new_hash|
-    new_hash.each do |info, array|
+    new_hash.each do |information, array|
       array.each do |name|
         pigeon_hierarchy[name] = {} if pigeon_hierarchy[name].nil?
         pigeon_hierarchy[name][category] = [] if pigeon_hierarchy[name][category].nil?
-        pigeon_hierarchy[name][category] << info.to_s
+        pigeon_hierarchy[name][category] << information.to_s
       end
     end
   end
