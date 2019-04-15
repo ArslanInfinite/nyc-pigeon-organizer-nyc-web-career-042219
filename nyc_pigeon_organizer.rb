40e32e353a6e39d5c04c15533a579d37117021dd
Arslan Ali <ali.arslan319@gmail.com>
2019-04-15 22:43:47 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,13 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  pigeon_hierarchy = {}
+  data.each do |category, new_hash|
+    new_hash.each do |info, array|
+      array.each do |name|
+        pigeon_hierarchy[name] = {} if pigeon_hierarchy[name].nil?
+        pigeon_hierarchy[name][category] = [] if pigeon_hierarchy[name][category].nil?
+        pigeon_hierarchy[name][category] << info.to_s
+      end
+    end
+  end
 end
+
